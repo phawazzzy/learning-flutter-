be102ed5951572025b6468e0a8fa318c31db4b77
kareem fawas <phawazzzy@gmail.com>
2020-04-15 18:06:15 +0100

    horizontal list

--- lib/main.dart
+++ lib/main.dart
@@ -3,27 +3,52 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  final title = 'Horizontal List Example';
   @override
   Widget build(BuildContext context) {
-    final title = 'List view';
-
     return MaterialApp(
       title: title,
       home: Scaffold(
           appBar: AppBar(
             title: Text(title),
           ),
-          body: ListView.separated(
-              itemCount: 50,
-              separatorBuilder: (BuildContext context, int index) => Divider(
-                    thickness: 2.0,
-                    color: Colors.blue,
+          body: ListView.builder(
+            itemCount: 4,
+            itemBuilder: (context, index) {
+            return Container(
+              margin: EdgeInsets.symmetric(vertical: 50.0),
+              height: 300.0,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: <Widget>[
+                  Container(
+                    width: 120.0,
+                    color: Colors.red,
                   ),
-              itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text("This is Position: $index"),
-                );
-              })),
+                  Container(
+                    width: 120.0,
+                    color: Colors.white,
+                  ),
+                  Container(
+                    width: 120.0,
+                    color: Colors.pinkAccent,
+                  ),
+                  Container(
+                    width: 120.0,
+                    color: Colors.green,
+                  ),
+                  Container(
+                    width: 120.0,
+                    color: Colors.pink,
+                  ),
+                  Container(
+                    width: 120.0,
+                    color: Colors.lime,
+                  )
+                ],
+              ),
+            );
+          })),
     );
   }
 }
